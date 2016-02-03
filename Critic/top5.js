f7f21b0ae9c0361779b33546fcccf2798dc37b95
Yurii Pereskokov <skulines@mail.ru>
2016-02-03 10:02:56 +0500

    02.02.2016/2

--- Critic/top5.js
+++ Critic/top5.js
@@ -22,5 +22,28 @@ function addToTop5()
 {
     var imgElement = this;
     var top5Element = document.getElementById('top5');
+    var numCDs = 0;
 
+    for (var i = 0; i < top5Element.childNodes.length; i++) {
+        if (top5Element.childNodes[i].nodeName.toLowerCase() == 'img') {
+            numCDs = numCDs + 1;
+        }
+    }
+
+    top5Element.appendChild(imgElement);
+    imgElement.onclick = null;
+
+    // Остальное читать не имеет смысла, это все делается на jq
 }
+
+// прописать в заголовке HTTP статус
+// header('Status: No address was received.', true, 400)
+
+// в js можно получить заголовок
+// request.getResponseHeader('Status');
+
+// При передачи ajax запроса таким способом в заголовке HTTP запроса нужно
+// указать в каком формате передаются данные, иначе сервер не сможет их прочесть.
+
+// Защита от sql запросов в PHP коде mysql_real_escape_string('строка');
+
